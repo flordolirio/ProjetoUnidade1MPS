bf70a40f243649d60828e862b350e69ce378b418
flordolirio <suzi.flordolirio@gmail.com>
2015-11-13 12:43:24 -0300

    Adicionado os padrões Comand e Fachada

--- src/view/UserAddForm.java
+++ src/view/UserAddForm.java
@@ -2,9 +2,13 @@ package view;
 
 import infra.InfraException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import business.control.FachadaComand;
 import business.control.UserManager;
 import business.model.User;
 import business.util.LoginInvalidException;
@@ -16,8 +20,11 @@ public class UserAddForm {
 	private Scanner opcao;
 	public boolean voltar = false;
 	UserManager userM;
+	FachadaComand userF;
+	List<String> dadosUser;
 
 	public UserAddForm(){
+		dadosUser = new ArrayList<String>();
 		userM = UserManager.getInstancia();
 	}
 	public void showMenuCadUsuario(){
@@ -38,16 +45,17 @@ public class UserAddForm {
 		switch (choice) {
 
 		case 1:
+			dadosUser.clear();
 			String nome = "";
 			String matricula = "";
 			String curso = "";
 			
 			System.out.println("Nome do usuario:");
-			nome = entrada.nextLine();
+			dadosUser.add(entrada.nextLine());
 			System.out.println("Matricula do usuario:");
-			matricula = entrada.nextLine();
+			dadosUser.add(entrada.nextLine());
 			System.out.println("Curso do usuario:");
-			curso = entrada.nextLine();
+			dadosUser.add(entrada.nextLine());
 			
 			while (checked) {
 				
@@ -55,63 +63,58 @@ public class UserAddForm {
 				String senha = "";
 				
 				System.out.println("Login do usuario (deve ter menos de 20 caracteres e não pode ter números):");
-				login = entrada.nextLine(); 
+				dadosUser.add(3, entrada.nextLine());
 				System.out.println("Senha do usuario (entre 8 e 12 caracteres e pelo menos 1 caracter e 2 números):");					
-				senha = entrada.nextLine();
+				dadosUser.add(4, entrada.nextLine());
 				
 				try {
-					userM.addUser(nome, matricula, curso, login, senha);
+					userF = new FachadaComand("add", dadosUser);
 					checked = false;
-				} catch (LoginInvalidException e) {
-					System.out.println(e.getMessage());
-				} catch (PasswordInvalidException e) {
-					System.out.println(e.getMessage());
-				} catch (InfraException e) {
-					System.out.println(e.getMessage());
-				} catch (IOException e) {
+				} catch (LoginInvalidException | PasswordInvalidException
+						| InfraException | IOException e) {
 					System.out.println(e.getMessage());
 				}
-				
 			}
 			showMenuCadUsuario();
 			break;
 		
 		case 2:
-			for (User user : userM.getUsers())
-				System.out.println("[ Nome: "+user.getNome()+" || Matricula: "+user.getMatricula()+ " || Curso: " +user.getCurso()+ " || Login: " + user.getLogin() + " || Senha: " + user.getSenha() + " ]");
+			try {
+				userF = new FachadaComand("lista", dadosUser);
+			} catch (LoginInvalidException | PasswordInvalidException
+					| InfraException | IOException e) {
+				System.out.println(e.getMessage());
+			}
 			
 			showMenuCadUsuario();
 			break;
 
 		case 3:
+			dadosUser.clear();
 			System.out.println("Login do usuario a ser excluido:");
-			String deleteLogin = entrada.nextLine();
+			dadosUser.add(entrada.nextLine());
+			
 			
 			try {
-				userM.deleteUser(deleteLogin);
-			} catch (LoginInvalidException e) {
-				System.out.println(e.getMessage());
-			} catch (InfraException e) {
-				System.out.println(e.getMessage());
-			} catch (IOException e) {
+				userF = new FachadaComand("delete", dadosUser);
+			} catch (LoginInvalidException | InfraException | PasswordInvalidException | IOException e) {
 				System.out.println(e.getMessage());
 			}
+			
 			showMenuCadUsuario();
 			break;
 			
 		case 4:
+			dadosUser.clear();
 			System.out.println("Login do usuario a ser atualizado:");
-			String atualizaLogin = entrada.nextLine();
-			
+			dadosUser.add(entrada.nextLine());
+		
 			try {
-				userM.atualizaUser(atualizaLogin);
-			} catch (LoginInvalidException e) {
-				System.out.println(e.getMessage());
-			} catch (InfraException e) {
-				System.out.println(e.getMessage());
-			} catch (IOException e) {
+				userF = new FachadaComand("update", dadosUser);
+			} catch (LoginInvalidException | PasswordInvalidException | InfraException | IOException e) {
 				System.out.println(e.getMessage());
 			}
+			
 			showMenuCadUsuario();
 			break;
 			
@@ -120,3 +123,4 @@ public class UserAddForm {
 		}
 	}
 }
+	
